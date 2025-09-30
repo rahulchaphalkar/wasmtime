1d564a7ebc9ad2f9dbeb70767b82391c1ced306d
Rahul Chaphalkar <rahul.s.chaphalkar@intel.com>
2025-09-30 16:03:28 -0700

    disable test with large memory alloc, very slow on sde

--- tests/all/gc.rs
+++ tests/all/gc.rs
@@ -1684,6 +1684,10 @@ fn instantiate_table_init_expr_oom() -> Result<()> {
 #[test]
 #[cfg_attr(miri, ignore)]
 fn instantiate_global_init_oom() -> Result<()> {
+    if wasmtime_test_util::is_sde() {
+        println!("skipping `instantiate_global_init_oom` test; memory allocation stress testing is extremely slow under SDE");
+        return Ok(());
+    }
     let mut config = Config::new();
     config.wasm_gc(true);
     config.wasm_function_references(true);
