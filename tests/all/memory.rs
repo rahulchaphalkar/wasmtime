6c907188dd172d7ecdafc53fce9260a61c992118
Rahul Chaphalkar <rahul.s.chaphalkar@intel.com>
2025-09-30 16:39:46 -0700

    disable more memory pool tests for sde

--- tests/all/memory.rs
+++ tests/all/memory.rs
@@ -195,6 +195,9 @@ fn guards_present() -> Result<()> {
 #[cfg_attr(miri, ignore)]
 #[cfg_attr(asan, ignore)]
 fn guards_present_pooling(config: &mut Config) -> Result<()> {
+    if std::env::var("WASMTIME_TEST_NO_SDE").is_ok() {
+        return Ok(());
+    }
     const GUARD_SIZE: u64 = 65536;
 
     let mut pool = crate::small_pool_config();
@@ -255,6 +258,9 @@ fn guards_present_pooling(config: &mut Config) -> Result<()> {
 #[cfg_attr(asan, ignore)]
 #[cfg(target_arch = "x86_64")] // only platform with mpk
 fn guards_present_pooling_mpk(config: &mut Config) -> Result<()> {
+    if std::env::var("WASMTIME_TEST_NO_SDE").is_ok() {
+        return Ok(());
+    }
     if !wasmtime::PoolingAllocationConfig::are_memory_protection_keys_available() {
         println!("skipping `guards_present_pooling_mpk` test; mpk is not supported");
         return Ok(());
