9e78680953dfffaeb37b0945e9e475ffde99e8e0
Rahul Chaphalkar <rahul.s.chaphalkar@intel.com>
2025-09-30 14:35:24 -0700

    sde ignore async timing tests, too slow on SDE emulation, prtest:sde

--- crates/misc/component-async-tests/tests/scenario/post_return.rs
+++ crates/misc/component-async-tests/tests/scenario/post_return.rs
@@ -36,6 +36,10 @@ pub async fn async_post_return_caller() -> Result<()> {
 
 #[tokio::test]
 pub async fn async_sleep_post_return_caller() -> Result<()> {
+    if wasmtime_test_util::is_sde() {
+        println!("skipping `async_sleep_post_return_caller` test; async timing operations are unreliable under SDE");
+        return Ok(());
+    }
     test_sleep_post_return(&[
         test_programs_artifacts::ASYNC_SLEEP_POST_RETURN_CALLER_COMPONENT,
         test_programs_artifacts::ASYNC_SLEEP_POST_RETURN_CALLEE_COMPONENT,
@@ -45,6 +49,10 @@ pub async fn async_sleep_post_return_caller() -> Result<()> {
 
 #[tokio::test]
 pub async fn async_sleep_post_return_callee() -> Result<()> {
+    if wasmtime_test_util::is_sde() {
+        println!("skipping `async_sleep_post_return_callee` test; async timing operations are unreliable under SDE");
+        return Ok(());
+    }
     test_sleep_post_return(&[test_programs_artifacts::ASYNC_SLEEP_POST_RETURN_CALLEE_COMPONENT])
         .await
 }
